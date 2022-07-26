4188be1cc46197e59c44271a0b4393fb651c73c7
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-07-26 15:30:35 +0530

    final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BsGithub } from 'react-icons/bs'
-import { SiLeetcode } from 'react-icons/si'
+import { SiLeetcode, SiInstagram } from 'react-icons/si'
 
 const Footer = () => {
   return (
     <div className='footer-container'>
         <div className="footer-Icon">
-            <BsGithub/>
-            <SiLeetcode/>
-        </div>
-        <div className='contatcs'>
-            <span> Email: Rashsih970@gmail.com</span>
+           <a href="https://github.com/7flash-pi">  <BsGithub /> </a>
+            <a href="https://leetcode.com/7lash/"><SiLeetcode className='icons-margin'/></a>
+            <a href="#"><SiInstagram/></a>
+            <span>Email: Rashsih970@gmail.com</span>
+            <span>Github: https://github.com/7flash-pi</span>
         </div>
       
     </div>
